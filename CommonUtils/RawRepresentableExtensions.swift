6b04fc60fcaa9195f1b22bfbe8afe5155298cecf
Isaac Charny <isaac.charny@gmail.com>
2017-08-24 21:28:00 -0400

    Add RawRepresentable extension

---
+++ CommonUtils/RawRepresentableExtensions.swift
@@ -0,0 +1,7 @@
+extension RawRepresentable {
+    init?(rawValue: Self.RawValue?) {
+        guard let rawValue = rawValue else { return nil }
+        
+        self.init(rawValue: rawValue)
+    }
+}
